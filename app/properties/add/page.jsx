0d105639ecf944a11dbc2b4d3c3433679cf32a44
Kiran <km060360@gmail.com>
2024-05-01 12:52:16 +0530

    NavCompleted

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const addpropertiesPage = () => {
+	return <div>addpropertiesPage</div>;
+};
+
+export default addpropertiesPage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const propertiesPage = () => {
+	return <div>propertiesPage</div>;
+};
+
+export default propertiesPage;
