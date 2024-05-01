82942d1ef41e9ef249665546b2f1f8e1b2a01fc3
Kiran <km060360@gmail.com>
2024-05-01 16:29:35 +0530

    Home Page and Properties

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const propertyId = () => {
+	return <div>propertyId</div>;
+};
+
+export default propertyId;
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
 
 const propertiesPage = () => {
-	return <div>propertiesPage</div>;
+	return (
+		<section className='px-4 py-6'>
+			<div className='container-xl lg:container m-auto px-4 py-6'>
+				{properties.legth === 0 ? (
+					<h1>No Properties</h1>
+				) : (
+					<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+						{properties.map((propertie) => {
+							return (
+								<PropertyCard
+									key={propertie.id}
+									property={propertie}
+								/>
+							);
+						})}
+					</div>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export default propertiesPage;
